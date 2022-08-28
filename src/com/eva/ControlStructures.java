cb9eb9ea0c1bc03491c3f66a0cabbe2522b1e68e
evafi <evafilgueirasaxo@gmail.com>
2022-08-28 12:21:53 +0200

    init commit

---
+++ src/com/eva/ControlStructures.java
@@ -0,0 +1,13 @@
+package com.eva;
+
+public class ControlStructures {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Robin", "John", "Marian", "Tuck"};
+
+        for(String name : names){
+            System.out.print(name.concat(" "));
+        }
+    }
+}
